04c25f44bfb12fced126d47aaf6ee7c69d0ebfd0
Oleksandr Buhaiov <oleksandr.buhaiov@gmail.com>
2026-06-18 09:50:38 +0300

    chore: update spring boot version

--- src/main/java-generated/com/epam/aidial/evaluation/data/db/jooq/analytics/tables/TestCaseRunResults.java
+++ src/main/java-generated/com/epam/aidial/evaluation/data/db/jooq/analytics/tables/TestCaseRunResults.java
@@ -22,13 +22,14 @@ import org.jooq.PlainSQL;
 import org.jooq.QueryPart;
 import org.jooq.SQL;
 import org.jooq.Schema;
-import org.jooq.Select;
 import org.jooq.Stringly;
 import org.jooq.Table;
 import org.jooq.TableField;
+import org.jooq.TableLike;
 import org.jooq.TableOptions;
 import org.jooq.UniqueKey;
 import org.jooq.impl.DSL;
+import org.jooq.impl.Internal;
 import org.jooq.impl.SQLDataType;
 import org.jooq.impl.TableImpl;
 
@@ -255,7 +256,7 @@ public class TestCaseRunResults extends TableImpl<TestCaseRunResultsRecord> {
      */
     @Override
     public TestCaseRunResults where(Condition condition) {
-        return new TestCaseRunResults(getQualifiedName(), aliased() ? this : null, null, condition);
+        return new TestCaseRunResults(getQualifiedName(), aliased() ? this : null, null, Internal.condition(this, condition));
     }
 
     /**
@@ -322,7 +323,7 @@ public class TestCaseRunResults extends TableImpl<TestCaseRunResultsRecord> {
      * Create an inline derived table from this table
      */
     @Override
-    public TestCaseRunResults whereExists(Select<?> select) {
+    public TestCaseRunResults whereExists(TableLike<?> select) {
         return where(DSL.exists(select));
     }
 
@@ -330,7 +331,7 @@ public class TestCaseRunResults extends TableImpl<TestCaseRunResultsRecord> {
      * Create an inline derived table from this table
      */
     @Override
-    public TestCaseRunResults whereNotExists(Select<?> select) {
+    public TestCaseRunResults whereNotExists(TableLike<?> select) {
         return where(DSL.notExists(select));
     }
 }
